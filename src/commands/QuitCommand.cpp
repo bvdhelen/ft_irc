1730246d364768d48042aad150566d089b4b9970
bvdhelen <elenadiazcontreras13@gmail.com>
2026-08-24 00:40:41 +0200

    Quit

--- src/commands/QuitCommand.cpp
+++ src/commands/QuitCommand.cpp
@@ -15,17 +15,16 @@ void QuitCommand::execute(Server &server, Client &client)
     std::set<Channel *>::const_iterator it;
 
     message = ":" + client.getNickname() + "!"
-        + client.getUsername() + " QUIT\r\n";
+        + client.getUsername() + "@"
+        + client.getHost() + " QUIT";
     if (!_params.empty())
-        message += " " + _params[0];
+        message += " :" + _params[0];
     message += "\r\n";
     it = client.getChannels().begin();
     while (it != client.getChannels().end())
     {
-        server.sendToChannelExcept(*it, &client, message);
+        server.sendToChannelExceptRaw(*it, &client, message);
         ++it;
     }
-    client.disconnect();
-    server.removeEmptyChannels();
     client.setRequestedDisconnection(true);
 }
