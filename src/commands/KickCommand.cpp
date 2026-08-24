ec63f1a4dcb6b05b2da543cccc88a5027840db17
bvdhelen <elenadiazcontreras13@gmail.com>
2026-08-25 01:35:46 +0200

    Topic & Kick Commands (#14)
    Co-authored-by: izhans <izhansastre@gmail.com>

---
+++ src/commands/KickCommand.cpp
@@ -0,0 +1,86 @@
+#include "commands/KickCommand.hpp"
+
+KickCommand::KickCommand(const std::vector<std::string> &params)
+{
+	_params = params;
+}
+
+KickCommand::~KickCommand(void)
+{
+}
+
+void KickCommand::kickClient(Server &server, Client &client, const std::string &channelName, const std::string &nickname)
+{
+	Channel *channel;
+	Client *target;
+	std::string message;
+
+	channel = server.getChannelByName(channelName);
+	if (channel == NULL)
+	{
+		server.sendReplyToClient(
+			&client,
+			ERR_NOSUCHCHANNEL,
+			"No such channel");
+		return ;
+	}
+	if (!client.isInChannel(channel))
+	{
+		server.sendReplyToClient(
+			&client,
+			ERR_NOTONCHANNEL,
+			"You're not on that channel");
+		return ;
+	}
+	if (!channel->isOperator(&client))
+	{
+		server.sendReplyToClient(
+			&client,
+			ERR_CHANOPRIVSNEEDED,
+			"You're not channel operator");
+		return ;
+	}
+	target = server.getClientByNick(nickname);
+	if (target == NULL)
+	{
+		server.sendReplyToClient(
+			&client,
+			ERR_NOSUCHNICK,
+			"No such nick");
+		return ;
+	}
+	if (!target->isInChannel(channel))
+	{
+		server.sendReplyToClient(
+			&client,
+			ERR_USERNOTINCHANNEL,
+			"They are not on that channel");
+		return ;
+	}
+	message = ":" + client.getNickname() + "!"
+		+ client.getUsername() + "@"
+    	+ client.getHost() + " KICK "
+		+ channel->getName() + " "
+		+ target->getNickname();
+    if (_params.size() > 2)
+        message += " :" + _params[2];
+    message += "\r\n";
+	server.sendToChannelRaw(channel, message);
+	target->removeChannel(channel);
+    if (channel->isEmpty())
+        server.removeChannel(channel->getName());
+}
+
+void KickCommand::execute(Server &server, Client &client)
+{
+    if (_params.size() < 2)
+	{
+		server.sendReplyToClient(
+			&client,
+			ERR_NEEDMOREPARAMS,
+			"Not enough parameters",
+			"KICK");
+		return ;
+	}
+	kickClient(server, client, _params[0], _params[1]);
+}
---
+++ src/commands/TopicCommand.cpp
@@ -0,0 +1,88 @@
+#include "commands/TopicCommand.hpp"
+
+TopicCommand::TopicCommand(const std::vector<std::string> &params)
+{
+	_params = params;
+}
+
+TopicCommand::~TopicCommand(void)
+{
+}
+
+void TopicCommand::sendCurrentTopic(Server &server, Client &client, Channel *channel)
+{
+	if (channel->getTopic().empty())
+	{
+		server.sendReplyToClient(
+			&client,
+			RPL_NOTOPIC,
+			"No topic is set");
+		return ;
+	}
+	server.sendReplyToClient(
+		&client,
+		RPL_TOPIC,
+		channel->getTopic(),
+		channel->getName());
+}
+
+void TopicCommand::setTopic(Server &server, Client &client, Channel *channel, const std::string &topic)
+{
+	std::string message;
+
+	if (channel->isProtectedTopic()
+		&& !channel->isOperator(&client))
+	{
+		server.sendReplyToClient(
+			&client,
+			ERR_CHANOPRIVSNEEDED,
+			"You're not channel operator");
+		return ;
+	}
+	channel->setTopic(topic);
+	message = ":" + client.getNickname() + "!"
+		+ client.getUsername() + "@"
+    	+ client.getHost() + " TOPIC "
+		+ channel->getName() + " :" + topic + "\r\n";
+	server.sendToChannelRaw(channel, message);
+}
+
+void TopicCommand::execute(Server &server, Client &client)
+{
+	Channel *channel;
+
+	if (_params.empty())
+	{
+		server.sendReplyToClient(
+			&client,
+			ERR_NEEDMOREPARAMS,
+			"Not enough parameters",
+			"TOPIC");
+		return ;
+	}
+	if (_params.size() > 2)
+		return ;
+	channel = server.getChannelByName(_params[0]);
+	if (channel == NULL)
+	{
+		server.sendReplyToClient(
+			&client,
+			ERR_NOSUCHCHANNEL,
+			"No such channel");
+		return ;
+	}
+	if (!channel->hasClient(&client))
+	{
+		server.sendReplyToClient(
+			&client,
+			ERR_NOTONCHANNEL,
+			"You're not on that channel");
+		return ;
+	}
+	if (_params.size() == 1)
+	{
+		sendCurrentTopic(server, client, channel);
+		return ;
+	}
+	setTopic(server, client, channel, _params[1]);
+}
