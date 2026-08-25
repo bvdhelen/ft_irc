c9791011a4ee1f4ffe06ccb46e492ac61cae8769
rubb20 <104703633+rubb20@users.noreply.github.com>
2026-08-25 05:10:36 +0200

    Register commands (PASS, NICK, USER) (#23)
    Co-authored-by: bvdhelen <elenadiazcontreras13@gmail.com>

--- src/parser/CommandFactory.cpp
+++ src/parser/CommandFactory.cpp
@@ -1,4 +1,7 @@
 #include "parser/CommandFactory.hpp"
+#include "commands/PassCommand.hpp"
+#include "commands/UserCommand.hpp"
+#include "commands/NickCommand.hpp"
 #include "commands/PingCommand.hpp"
 #include "commands/PartCommand.hpp"
 #include "commands/QuitCommand.hpp"
@@ -14,9 +17,9 @@ Command *CommandFactory::createCommand(std::string commandName, const std::vecto
 
 	// TODO: add/uncomment commands as they are implemented
 	// if (commandName == "JOIN")		return new JoinCommand(params);
-	// if (commandName == "NICK")		return new NickCommand(params);
-	// if (commandName == "USER")		return new UserCommand(params);
-	// if (commandName == "PASS")		return new PassCommand(params);
+	if (commandName == "NICK")			return new NickCommand(params);
+	if (commandName == "USER")			return new UserCommand(params);
+	if (commandName == "PASS")			return new PassCommand(params);
 	// if (commandName == "PRIVMSG")	return new PrivmsgCommand(params);
 	if (commandName == "PART")			return new PartCommand(params);
 	if (commandName == "QUIT")			return new QuitCommand(params);
